fce03f6c4f87094d4bd5562df8d9a3dcc175d88b
Bombata Habeeb <bombatahabeeb@Bombatas-MacBook-Pro.local>
2022-06-07 19:02:30 +0100

    Receiver Functions with Pointers

--- bill.go
+++ bill.go
@@ -14,14 +14,14 @@ func newBill(name string) bill {
 	b := bill{
 
 		name:  name,
-		items: map[string]float64{"pie": 5.99, "cake": 3.99},
+		items: map[string]float64{},
 		tip:   0,
 	}
 	return b
 }
 
 // format the bill
-func (b bill) format() string {
+func (b *bill) format() string {
 	fs := "Bill breakdown: \n"
 	var total float64 = 0
 
@@ -32,8 +32,23 @@ func (b bill) format() string {
 		total += v
 	}
 
+	//add tip output
+	fs += fmt.Sprintf("%-25v ...$%v\n", "tip:", b.tip)
+
 	//total
-	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total)
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total+b.tip)
 
 	return fs
+}
+
+//update tip
+
+func (b *bill) updateTip(tip float64) {
+	b.tip = tip
+
+}
+
+// add an item to the bill
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
 }
